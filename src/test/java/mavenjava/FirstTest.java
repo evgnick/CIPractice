705ef3ee2da391b90ef760ac8a1970ad976fe680
mihaibordei <workbordeimihai@gmail.com>
2024-11-25 20:39:42 -0800

    changed yaml file

--- src/test/java/mavenjava/FirstTest.java
+++ src/test/java/mavenjava/FirstTest.java
@@ -10,6 +10,7 @@ public class FirstTest extends BaseTest {
     public void testStoreName() throws InterruptedException {
 
         String websiteName = driver.findElement(By.xpath("//h1/a[@href='https://askomdch.com/']")).getText();
+
         Assert.assertEquals(websiteName, "AskOmDch");
 
         Thread.sleep(3000);
